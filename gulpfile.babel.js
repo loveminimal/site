2b5c860f3401b7a7144b9b26e00a8e49ef55e770
Jack Liu <loveminimal@163.com>
2021-01-01 11:56:35 +0800

    Add css task.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,25 @@
+const { src, dest, series, parallel } = require("gulp");
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify");
+
+const sass = require("gulp-dart-sass");
+
 function defaultTask(cb) {
     // ...
+    return src("./src/main.js")
+        .pipe(
+            babel({
+                presets: ["@babel/env"],
+            })
+        )
+        .pipe(uglify())
+        .pipe(dest("dist"));
+
     cb();
 }
 
-module.exports.default = defaultTask;
+function css(cb) {
+    return src("./src/style.scss").pipe(sass()).pipe(dest('dist'));
+}
+
+module.exports.default = parallel(css);
