22303f86e2e9fb53d943eecb1cb42233f6bbfa12
Jack Liu <loveminimal@163.com>
2021-01-25 12:02:02 +0800

    Add gulp server.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,18 @@
-const { src, dest, series, parallel, watch } = require("gulp");
-const babel = require("gulp-babel");
-const uglify = require("gulp-uglify");
-const browserify = require("gulp-browserify");
+const { src, dest, series, parallel, watch } = require('gulp');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const browserify = require('gulp-browserify');
 
-const sass = require("gulp-dart-sass");
+const sass = require('gulp-dart-sass');
+
+const gls = require('gulp-live-server');
 
 function js(cb) {
     // ...
-    return src("./src/main.js")
+    return src('./src/main.js')
         .pipe(
             babel({
-                presets: ["@babel/env"],
+                presets: ['@babel/env'],
             })
         )
         .pipe(
@@ -20,15 +22,15 @@ function js(cb) {
             })
         )
         .pipe(uglify())
-        .pipe(dest("dist"));
+        .pipe(dest('dist'));
 
     cb();
 }
 
 function css(cb) {
-    return src("./src/style.scss")
-        .pipe(sass({ outputStyle: "compressed" }))
-        .pipe(dest("dist"));
+    return src('./src/style.scss')
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(dest('dist'));
 
     cb();
 }
@@ -37,12 +39,22 @@ function css(cb) {
 //     cb();
 // });
 
+function devServer(cb) {
+    let server = gls.static('public', 3000);
+    server.start();
+
+    // watch('public/**/*', (file) => server.notify.apply(server, [file]))
+    cb();
+}
+
 module.exports.default = function () {
     watch(
-        "src/**/*.(js|scss)",
-        { events: "all", queue: false, delay: 500 },
+        'src/**/*.(js|scss)',
+        { events: 'all', queue: false, delay: 500 },
         parallel(js, css)
     );
 };
 
 module.exports.build = parallel(js, css);
+
+module.exports.dev = devServer;
