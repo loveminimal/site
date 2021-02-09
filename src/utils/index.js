0c78f884765909b1d13d5b771e23868621e1c93d
jack <loveminimal@163.com>
2021-02-09 14:53:52 +0800

    Better utils.

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,76 +1,76 @@
-module.exports = {
-    // Diff device type
-    browserRedirect() {
-        let sUserAgent = navigator.userAgent.toLowerCase();
-        let bIsIpad = sUserAgent.match(/ipad/i) == 'ipad';
-        let bIsIphoneOs = sUserAgent.match(/iphone os/i) == 'iphone os';
-        let bIsMidp = sUserAgent.match(/midp/i) == 'midp';
-        let bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == 'rv:1.2.3.4';
-        let bIsUc = sUserAgent.match(/ucweb/i) == 'ucweb';
-        let bIsAndroid = sUserAgent.match(/android/i) == 'android';
-        let bIsCE = sUserAgent.match(/windows ce/i) == 'windows ce';
-        let bIsWM = sUserAgent.match(/windows mobile/i) == 'windows mobile';
+// Diff device type
+function browserRedirect() {
+    let sUserAgent = navigator.userAgent.toLowerCase();
+    let bIsIpad = sUserAgent.match(/ipad/i) == 'ipad';
+    let bIsIphoneOs = sUserAgent.match(/iphone os/i) == 'iphone os';
+    let bIsMidp = sUserAgent.match(/midp/i) == 'midp';
+    let bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == 'rv:1.2.3.4';
+    let bIsUc = sUserAgent.match(/ucweb/i) == 'ucweb';
+    let bIsAndroid = sUserAgent.match(/android/i) == 'android';
+    let bIsCE = sUserAgent.match(/windows ce/i) == 'windows ce';
+    let bIsWM = sUserAgent.match(/windows mobile/i) == 'windows mobile';
 
-        if (
-            bIsIpad ||
-            bIsIphoneOs ||
-            bIsMidp ||
-            bIsUc7 ||
-            bIsUc ||
-            bIsAndroid ||
-            bIsCE ||
-            bIsWM
-        ) {
-            return 'MB';
-        } else {
-            return 'PC';
-        }
-    },
+    if (
+        bIsIpad ||
+        bIsIphoneOs ||
+        bIsMidp ||
+        bIsUc7 ||
+        bIsUc ||
+        bIsAndroid ||
+        bIsCE ||
+        bIsWM
+    ) {
+        return 'MB';
+    } else {
+        return 'PC';
+    }
+}
 
-    // If `args` is current page
-    // `args` - String or Array
-    isCurPageFn(args) {
-        if (typeof args === 'string') {
-            if (
-                [
-                    '/public/' + args + '.html',
-                    '/public/' + args,
-                    '/' + args + '.html',
-                    '/' + args,
-                ].includes(location.pathname)
-            )
-                return true;
-        } else if (args instanceof Array) {
-            let _res = 0;
-            args.map((item) => {
-                if (
-                    [
-                        '/public/' + item + '.html',
-                        '/public' + item,
-                        '/' + item + '.html',
-                        '/' + item,
-                    ].includes(location.pathname)
-                )
-                    _res += 1;
-            });
-
-            if (_res > 0) return true;
-        }
-    },
-    isHomeFn() {
+// If `args` is current page
+// `args` - String or Array
+function isCurPageFn(args) {
+    if (typeof args === 'string') {
         if (
             [
-                '/public/index.html',
-                '/public/index',
-                '/public/',
-                '/index.html',
-                '/index',
-                '/',
+                '/public/' + args + '.html',
+                '/public/' + args,
+                '/' + args + '.html',
+                '/' + args,
             ].includes(location.pathname)
-        ) {
+        )
             return true;
-        } else {
-            return false;
-        }
-    },
-};
+    } else if (args instanceof Array) {
+        let _res = 0;
+        args.map((item) => {
+            if (
+                [
+                    '/public/' + item + '.html',
+                    '/public' + item,
+                    '/' + item + '.html',
+                    '/' + item,
+                ].includes(location.pathname)
+            )
+                _res += 1;
+        });
+
+        if (_res > 0) return true;
+    }
+}
+function isHomeFn() {
+    if (
+        [
+            '/public/index.html',
+            '/public/index',
+            '/public/',
+            '/index.html',
+            '/index',
+            '/',
+        ].includes(location.pathname)
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+module.exports = { browserRedirect, isCurPageFn, isHomeFn };
