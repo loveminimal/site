3c0c0a85d750e3ee463ae88f4f516605dd16f6bb
jack <loveminimal@163.com>
2021-02-24 18:57:55 +0800

    Deploy posts to loveminimal.github.io.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
+# -------------------
+# Deploy posts to `loveminimal.github.io`
+# -------------------
 
 if [ -d "public" ]
 then
-
-cp -r public temp
-
-if
+    cp -r "public" "../.temp"
+    cd "../.temp"
+    pwd
+    git init
+    git add .
+    git commit -m "Posts update."
+    git remote add origin https://github.com/loveminimal/loveminimal.github.io.git
+    git push -f origin master:main
+    cd ..
+    rm -rf ".temp"
+    cd "site"
+fi
